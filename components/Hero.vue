2ba54ac4d60f20d68e02169466cc3269d7bcf41a
ArthurJfr <jaffro.arthur@gmail.com>
2024-11-29 02:10:24 +0100

    🚀 massive add

---
+++ components/Hero.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="main-content">
+        <div class="main-text">
+            <h1 class="grad">Nos prédictions crypto</h1>
+            <p class="grad following">Boostées par IA !</p>
+            <div class="bottompart">
+                <p class="grad">Anticipez les mouvements du marché grâce à notre analyse avancée des sentiments</p>
+                <div class="bothbtn">
+                    <button class="btnreversed" @click="redirectToPred"><span>Voir les prédictions</span></button>
+                </div>   
+            </div>
+
+        </div>
+        <div class="imagehero">
+            <img src="/assets/heroimg.png" alt="">
+        </div>
+
+   </div>
+</template>
+<script setup>
+import { useRouter } from 'vue-router';
+ const router = useRouter();
+
+
+ const redirectToPred = () => {
+    router.push('/predictions');
+ }
+</script>
+<style scoped>
+.main-content {
+    margin-top: 8rem;
+    padding: 0 1rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+}
+.imagehero img  {
+    max-width: 600px;
+    filter: drop-shadow( 5px 15px 5px rgba(0,0,0,0.4));
+
+}
+.main-text {
+    display: flex;
+    flex-direction: column;
+}
+.main-text h1 {
+    font-size: 45px;
+    margin: 0;
+}
+.main-text .following {
+    font-size: 25px;
+    margin: 0;
+    font-weight: bold;
+}
+
+.main-text .following {
+    font-size: 20px;
+    margin: 0;
+    font-weight: bold;
+}
+
+.main-text .bothbtn {
+    display: flex;
+    gap: 3rem;
+}
+.main-text .bottompart {
+margin-top: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,24 @@
 <template>
     <div class="main-content">
-        <div class="main-text">
-            <h1 class="grad">Nos prédictions crypto</h1>
-            <p class="grad following">Boostées par IA !</p>
-            <div class="bottompart">
-                <p class="grad">Rejoignez notre newletters pour être au courant des derniers changement</p>
-                <div class="bothbtn">
-                    <button class="btnreversed" ><span>Rejoindre</span></button>
-                    <button class="btn" @click="redirectToPred"><span>Voir les prédictions</span></button>
-                </div>   
-            </div>
-
-        </div>
-        <div class="imagehero">
-            <img src="/assets/heroimg.png" alt="">
-        </div>
+        <Hero/>
+        <HomeSection/>
+        <Footer/>
    </div>
 </template>
 
 
 <script setup>
-import { useRoute, useRouter} from 'vue-router';
+import { useRouter } from 'vue-router';
+import HomeSection from '../components/HomeSection.vue';
+import Footer from '~/components/Footer.vue';
+
+import { ref, onMounted } from 'vue';
+import { getPredictions } from '~/services/api';
  const router = useRouter();
+ const predictions = ref([]);
 
  const redirectToPred = () => {
-    router.push('/predictions')
+    router.push('/predictions');
  }
 
 
@@ -32,46 +26,3 @@ import { useRoute, useRouter} from 'vue-router';
 
 
 
-<style scoped>
-.main-content {
-    margin-top: 6rem;
-    padding: 0 1rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-}
-.imagehero img  {
-    max-width: 500px;
-    filter: drop-shadow( 5px 15px 5px rgba(0,0,0,0.4));
-}
-.main-text {
-    display: flex;
-    flex-direction: column;
-}
-.main-text h1 {
-    font-size: 45px;
-    margin: 0;
-}
-.main-text .following {
-    font-size: 25px;
-    margin: 0;
-    font-weight: bold;
-}
-
-.main-text .following {
-    font-size: 20px;
-    margin: 0;
-    font-weight: bold;
-}
-
-.main-text .bothbtn {
-    display: flex;
-    gap: 1rem;
-}
-.main-text .bottompart {
-margin-top: 2rem;
-}
-
-
-</style>
